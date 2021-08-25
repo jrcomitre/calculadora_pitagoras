4c763c84dabe4788ea4a63d8b4ab230858ec0891
jrcomitre <gcomitrejr@gmail.com>
2021-08-25 19:45:53 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var cata = document.getElementById("cat-a");
+var catb = document.getElementById("cat-b");
+var hipc = document.getElementById("hip-c");
+var resp = document.getElementById("resultado");
+
+function result() {
+    var a = Number(cata.value) ** 2;
+    var b = Number(catb.value) ** 2;
+    var c = Number(hipc.value) ** 2;
+    
+}
